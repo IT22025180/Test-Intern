37cae198523487659af6b0cb5277105bd93fe5f2
IT22025180 <it22025180@my.sliit.lk>
2025-02-24 22:45:55 +0530

    Vanuja

--- Backend/Login/LoginController.js
+++ Backend/Login/LoginController.js
@@ -1,35 +1,37 @@
-const { Login , validate} = require('./LoginModel');
+const { Login, validate } = require('./LoginModel');
 const bcrypt = require('bcrypt');
 const Joi = require('joi');
 
-const addLogin = async(req,res) => {
-    try{
-        
-            const{error} = validate(req.body);
 
-            if(error){
-                return res.status(400).json({success : false , message : error.details[0].message});
-            }
+//create login
+const addLogin = async (req, res) => {
+    try {
 
-            const user = await Login.findOne({username : req.body.username});
-            if(user){
-                return res.status(409).json({ message : 'This user name is already exists'});
-            }
+        const { error } = validate(req.body);
 
-            const salt = await bcrypt.genSalt(Number(10));
-            const hashPassword = await bcrypt.hash(req.body.password , salt);
-            const newuser =  new Login({
-                
-                ...req.body,
-                password : hashPassword       
-            });
-
-            await newuser.save();
-            res.json({ success : true , message : 'User added successfully'});
+        if (error) {
+            return res.status(400).json({ success: false, message: error.details[0].message });
         }
-    catch(error){
-        console.error('Error adding user: ' , error);
-        res.status(500).json({ success : false , message : 'Internal server error'});
+
+        const user = await Login.findOne({ username: req.body.username });
+        if (user) {
+            return res.status(409).json({ message: 'This user name is already exists' });
+        }
+
+        const salt = await bcrypt.genSalt(Number(10));
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
+        const newuser = new Login({
+
+            ...req.body,
+            password: hashPassword
+        });
+
+        await newuser.save();
+        res.json({ success: true, message: 'User added successfully' });
+    }
+    catch (error) {
+        console.error('Error adding user: ', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
     }
 }
 
